4096f77f3a47ecbe53e675984c681f8c3c2633ef
Aman2205saini <137211234+Aman2205saini@users.noreply.github.com>
2025-01-22 15:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function getvalue(){
+	var fname=document.getElementsByName("fname")[0].value;
+	var lname=document.getElementsByName("lname")[0].value;
+	var phone=document.getElementsByName("phone")[0].value;
+	var email=document.getElementsByName("email")[0].value;
+
+	alert(`First Name: ${fname} Last Name: ${lname} Phone Number: ${phone} Email ID:${email}`)
+}
