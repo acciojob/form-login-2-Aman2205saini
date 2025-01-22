990d2c709a168774b58284372d59a18695a11054
Aman2205saini <137211234+Aman2205saini@users.noreply.github.com>
2025-01-22 22:31:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//your JS code here. If required.
 function getvalue(event){
     event.preventDefault();
-	var fname=document.getElementsByName("First Name")[0].value;
-	var lname=document.getElementsByName("Last Name")[0].value;
-	var phone=document.getElementsByName("Phone Number")[0].value;
-	var email=document.getElementsByName("Email ID")[0].value;
+    var fname = document.getElementsByName("firstName")[0].value;
+    var lname = document.getElementsByName("lastName")[0].value;
+    var phone = document.getElementsByName("phoneNumber")[0].value;
+    var email = document.getElementsByName("emailID")[0].value;
 
-	alert(`
-	First Name: ${fname}
-	Last Name: ${lname}
-	Phone Number: ${phone}
-	Email ID:${email}`)
+    alert(`
+        First Name: ${fname}
+        Last Name: ${lname}
+        Phone Number: ${phone}
+        Email ID: ${email}
+    `);
 }
